9e9a5bdad51d5fc4371dacfff1415666445cf4a0
Kirk Sefchik <1457993+deathgaze@users.noreply.github.com>
2020-05-06 12:58:56 -0500

    add support for multistage build

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,20 @@
-FROM ubuntu
+FROM node:12
+
+RUN mkdir /build
+WORKDIR /build
+
+COPY . .
+RUN npm ci
+RUN npm run bundle
+
+FROM ubuntu:18.04
+
+ENV NODE_ENV=production
+ENV SERVICEDIR=/snapshot/build/services
 
 RUN mkdir /app
 WORKDIR /app
 
-COPY dist/moleculer-pkg-demo .
+COPY --from=0 /build/dist/app .
 
-CMD ["./moleculer-pkg-demo"]
+CMD ["./app"]
--- start.js
+++ start.js
@@ -3,16 +3,12 @@ const Runner = require("moleculer").Runner;
 
 const runner = new Runner();
 
-if (process.env.NODE_ENV == "production") {
-	process.env.SERVICEDIR = "/snapshot/moleculer-pkg-demo/services";
-}
-
 runner.start([
 	process.argv[0],
 	__filename,
 	"--config",
 	path.join(__dirname, "moleculer.config.js"),
-	//"--repl",
+	// "--repl",
 	path.join(__dirname, "services")
 ]).catch(err => {
 	console.error(err);
